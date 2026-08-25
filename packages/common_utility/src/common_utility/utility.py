72378dcee227519dea51de17e1b5eb96f58e3b0d
Kunal Patil <kunal.patil.ve@mitwpu.edu.in>
2026-08-25 18:00:36 +0530

    add utility function

---
+++ packages/common_utility/src/common_utility/utility.py
@@ -0,0 +1,8 @@
+
+class Utility_functions:
+
+    def __init__(self):
+        pass
+    
+    def load_yaml_file(self,file_path):
+        print(f"Loading YAML file from: {file_path}")
--- services/churn_pipeline/src/churn_pipeline/data/ingestion.py
+++ services/churn_pipeline/src/churn_pipeline/data/ingestion.py
@@ -6,9 +6,12 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 from common_logging.factory import logger
+from common_utility.utility import Utility_functions
 
 
+helper = Utility_functions()
 
+helper.load_yaml_file("config.yaml")
 logging = logger
 
 logging.info("Starting data ingestion process...")
